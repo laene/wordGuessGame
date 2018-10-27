9a24d2cb259f43fc32dd91f11eef14870e59b3dd
Elena Henry <laene234@gmail.com>
2018-10-27 16:27:18 -0500

    added more words to array; functioning word blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,21 @@
 //object of arrays for word list
 var wins = 0;
 var losses = 0;
-var success = true;
+var complete = false;
 var letterGuess = " "
 var badGuesses = [];
 var newWord = "";
 
-var wordList = ["alligator"]
+var wordList = ["alligator", "gumbo", "beignets","jazz", "hurricane", "daquiri", "parades", "crawfish", "magnolia", "jambalaya", "lagniappe"]
+var current = 0;
 
 //Yay! I used correct syntax for my first word!
-console.log(wordList[0]);
+console.log(wordList[current]);
 
 //This needs to print __ for each letter. Ex: alligator = _ _ _ _ _ _ _ _ _ 
 function printBlanks() {
     console.log(wordList);
-    for (var i = 0; i < wordList[0].length; i++) {
+    for (var i = 0; i < wordList[current].length; i++) {
         console.log("_");
         newWord = newWord + "_ ";
     };
@@ -22,13 +23,15 @@ function printBlanks() {
 }
 
 function trackScore() {
-    if (success === true) {
+    if (complete === true) {
         wins++;
         console.log("New Score: " + wins + " wins!");
+        document.getElementById("wins").innerText = "Wins: " + wins;
     }
-    else if (success === false) {
+    else if (complete === false) {
         losses++;
         console.log("New Score: " + losses + " losses!");
+        document.getElementById("losses").innerText = "Losses: " + losses;
     }
 };
 
@@ -36,17 +39,20 @@ onkeyup = function () {
     printBlanks(wordList);
     onkeyup = function () {
         letterGuess = event.key;
-        var n = wordList[0].indexOf(letterGuess);
+        var n = wordList[current].indexOf(letterGuess);
 
         if (n >= 0) {
             console.log("yay!")
-            for (var i = 0; i < wordList[0].length; i++) {
-                l = wordList[0].charAt(i);
+            var pos = 0;
+            var oldWord = newWord;
+            for (var i = 0; i < wordList[current].length; i++) {
+                l = wordList[current].charAt(i);
                 if (l == letterGuess) {
                     console.log(i);
                     // var pos = newWord.charAt(i);
-                    // pos *= 2;
-                    newWord = newWord.replace(i, l);
+                    pos = i*2;
+                    newWord = oldWord.substr(0,pos) + l + " " + oldWord.substr(pos+2, oldWord.length+1);
+                    oldWord = newWord;
                     console.log(l);
                     console.log(newWord)
                 }
@@ -58,6 +64,11 @@ onkeyup = function () {
             console.log("boo :(")
             badGuesses.push(letterGuess);
             document.getElementById("guessedLetters").innerText = "You Already Guessed: " + badGuesses;
+        }
+        if (newWord.indexOf("_") < 0) {
+            complete = true;
+            trackScore();
+            
         }
     }
 
